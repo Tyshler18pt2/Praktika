fb7090d8b30f9950bb45e26f8a60dc30119b9f49
Tyshler18pt2 <skinnybot00@mail.ru>
2022-12-06 18:58:19 +0300

    Изменил файл Makefile

--- example-1/hello.cpp
+++ example-1/hello.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    if (argc == 1 ) {
-        cout<<"Hello! I can output my command line parameters. Try this\n";
-    } else {
-        for (int i=0; i<argc; i++) {
-            cout<<i<<'\t'<<argv[i]<<endl;
+    if (argc == 1)
+    {
+        cout << " Hello! I can output my command line parameters. Try this\n";
+    }
+    else
+    {
+        for (int i = 0; i < argc; i++)
+        {
+            cout << i << '\t' << argv[i] << endl;
         }
     }
     return 0;
